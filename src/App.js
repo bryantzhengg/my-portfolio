3832b750ecc02f2e9f95d4caa58792021a8ff4f5
bryantzhengg <bryant.zhg@gmail.com>
2023-01-19 21:00:05 -0500

    3rd

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/my-portfolio" element={<Home/>}/>
+        <Route path="/my-portfolio/" element={<Home/>}/>
       </Routes>
     </>
   );
